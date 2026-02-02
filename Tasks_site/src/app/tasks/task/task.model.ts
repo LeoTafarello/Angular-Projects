8d7717090b5e89c0e2baf9faff937853872ee25f
Leonardo Tafarello <Leonardo.tafarello@hotmail.com>
2026-02-02 20:12:55 -0300

    Reorganize: Move Tasks_site to Tasks_Site folder

--- Tasks_site/src/app/tasks/task/task.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Task{   // exporta a interface Task para ser usada em outros arquivos, pois se tiver algo de task tem que estar na pasta task
-  id: string;
-  userId: string;
-  title:  string;
-  summary: string;
-  dueDate: string;
-}
