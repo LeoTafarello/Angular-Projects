f380e45f6e46bd1519a2fc601cf3be856c072671
Leonardo Tafarello <Leonardo.tafarello@hotmail.com>
2026-02-03 21:20:44 -0300

    New updates about Template Variables

--- server-site/Server-site/src/app/dashboard/server-status/server-status.component.ts
+++ server-site/Server-site/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit,inject,DestroyRef } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit{  //usando o implements faz com que eu precise implementar essa interface, se eu errar o nome dara erro
+export class ServerStatusComponent implements OnInit {  //usando o implements faz com que eu precise implementar essa interface, se eu errar o nome dara erro... Se for usar o metodo antigo do OnDestroy, adicionalo aki
 
-   currentStatus: 'online' | 'offline' | 'unknown' = 'online';  //estou falando que a variavel so pode receber o status online,offline ou uknown, e no caso agora seria online. Caso use pra frente e nao for igual, dara erro
+  //private interval?: NodeJS.Timeout; //interval vai receber o valor do setInterval para ser usado no ngOnDestroy, caso nao seja mais utilizado.
+  //private interval?: ReturnType<typeof setInterval>; //mudando para este modo pois se tiver esse erro -> Cannot find namespace 'NodeJS'.
+  private destroyRef = inject(DestroyRef); //usando DestroyRef que é uma classe do Angular e faz a mesma coisa dos de cima, so que mais "atualizado" e so funciona no Angular v16 pra cima
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';  //estou falando que a variavel so pode receber o status online,offline ou uknown, e no caso agora seria online. Caso use pra frente e nao for igual, dara erro
 
   ngOnInit(){  //Usamos o ngOnInit para rodar depois de receber o input. Lembrando que para HTTP, devemos usar sempre o ngOnInit
-    setInterval(() => {
+    // this.interval = setInterval(() => {   //pode usar para o metodo antigo que seria o ngOnDestroy
+    const interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd < 0.5){
         this.currentStatus = 'online';
@@ -22,6 +26,15 @@ export class ServerStatusComponent implements OnInit{  //usando o implements faz
         this.currentStatus = 'unknown'
       }
     }, 5000);
+
+    this.destroyRef.onDestroy (() => {
+      clearInterval(interval);
+    });
+
   }
+
+  // ngOnDestroy(){ //usando ngOnDestroy para parar limpar o setIterval caso nao use mais, pois se não tera mais uso de memoria e ter impacto negativo na aplicação
+  //   clearTimeout(this.interval);
+  // }
 
 }
--- server-site/Server-site/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ server-site/Server-site/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent,FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+
+  //OnSubmit(tittleElement: HTMLInputElement){  //peguei o tipo passando o mouse em cima da variavel #titleInput
+    //console.dir(tittleElement.value); //console.log vai retornar 1 JS objetc, por isso usar DIR para mostrar Object que da pra ver as propriedades
+  //}
+
+  OnSubmit (title:string, ticketText:string){  //usando title como 1 valor passado do ngSubmit e ticket como 2 valor e falando que serão strings
+    console.log(title);
+    console.log(ticketText);
+  }
 
 }
